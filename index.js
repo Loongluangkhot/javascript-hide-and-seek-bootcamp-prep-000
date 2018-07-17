a0318a30f87cd7fb903304fdb54e1916b3110e16
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-17 15:51:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  let rankedList = document.querySelectorAll('.ranked-list li')
+  for(let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+// use breath-first-search algorithm and return 'past' when 'next' is exhausted, ie 'current' is undefined. 'past' returned is the deepest child / lowest on the NodeTree
+
+  let past = 0
+  let current = document.querySelector("div#grand-node")
+  let next = []
+
+  while(current) {
+
+    // if 'current' has children, push to end of 'next' queue
+    if(current.children) {
+      for(let i = 0; i < current.children.length; i++) {
+        next.push(current.children[i])
+      }
+    }
+    
+    // take the first element in queue 'next'
+    past = current
+    current = next.shift()
+  }
+
+  return past
+}
